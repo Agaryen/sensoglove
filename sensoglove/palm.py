8ee724d60ec9ce88ecebf06c1e7126b701e695d3
Thibault Miranda de Oliveira <thibault@mirandadeoliveira.com>
2018-07-01 19:35:55 +0100

    Use splat in palm object

--- sensoglove/palm.py
+++ sensoglove/palm.py
@@ -9,7 +9,7 @@ class Speed:
 class Palm:
     def __init__(self, data):
         palm = data['data']['palm']
-        self.rotation = Rotation(palm['quat'][1], palm['quat'][2], palm['quat'][3])
-        self.speed = Speed(palm['spd'][0], palm['spd'][1], palm['spd'][2])
-        self.acceleration = Speed(palm['acc'][0], palm['acc'][1], palm['acc'][2])
+        self.rotation = Rotation(*palm['quat'][1:])
+        self.speed = Speed(*palm['spd'])
+        self.acceleration = Speed(*palm['acc'])
         self.accuration = palm['accuracy']
