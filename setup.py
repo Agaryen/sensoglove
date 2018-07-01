8910b4402762e5bb2dca68e4cf4627ab63eface5
Thibault Miranda de Oliveira <thibault@mirandadeoliveira.com>
2018-07-01 18:49:41 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='sensoglove',
+      version='0.1',
+      description='Wrapper module for senso gloves',
+      url='https://github.com/TheRizzen/sensoglove',
+      author='Thibault Miranda de Oliveira',
+      author_email='thibault@mirandadeoliveira.com',
+      license='MIT',
+      packages=['sensoglove'])
