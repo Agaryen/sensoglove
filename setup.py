eb15d3a08350877174cd62b75beacec8f72e12af
Thibault Miranda de Oliveira <thibault@mirandadeoliveira.com>
2018-08-15 21:50:44 +0200

    Edit setup.py for v0.2

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-from setuptools import setup
+import setuptools
 
-setup(name='sensoglove',
-      version='0.1',
+setuptools.setup(name='sensoglove',
+      version='0.2',
       description='Wrapper module for senso gloves',
       url='https://github.com/TheRizzen/sensoglove',
       author='Thibault Miranda de Oliveira',
       author_email='thibault@mirandadeoliveira.com',
       license='MIT',
-      packages=['sensoglove'])
+      packages=setuptools.find_packages(),
+      classifiers=(
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ),
+)
